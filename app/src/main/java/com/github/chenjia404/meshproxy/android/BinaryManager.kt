a0844cd473de2b635d63fffea9a4af97085f5efc
chenjia404 <chenjiablog@gmail.com>
2026-03-19 23:16:16 +0800

    优化启动参数，避免冲突

--- app/src/main/java/com/github/chenjia404/meshproxy/android/BinaryManager.kt
+++ app/src/main/java/com/github/chenjia404/meshproxy/android/BinaryManager.kt
@@ -7,6 +7,9 @@ import java.io.IOException
 class BinaryManager(private val context: Context) {
 
     private val binaryName = "libmeshproxy.so"
+    private val p2pListenAddrsArg =
+        "--p2p.listen_addrs=/ip4/0.0.0.0/tcp/4002,/ip6/::/tcp/4002"
+    private val socks5ListenArg = "--socks5.listen=127.0.0.1:1082"
     private val binaryFile: File by lazy {
         File(context.applicationInfo.nativeLibraryDir, binaryName)
     }
@@ -24,7 +27,7 @@ class BinaryManager(private val context: Context) {
 
         return try {
             binaryFile.setExecutable(true, true)
-            ProcessBuilder(binaryFile.absolutePath)
+            ProcessBuilder(binaryFile.absolutePath, p2pListenAddrsArg, socks5ListenArg)
                 .directory(context.filesDir)
                 .redirectErrorStream(true)
                 .start()
